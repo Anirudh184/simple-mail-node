e7c64ac22be2e335588597358327fb6f500a6ba0
Anirudha sah <anirudha.s@simformsolutions.com>
2020-08-30 12:56:51 -0700

    changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const transporter = require('./mailtransporter');
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('API service is live');
